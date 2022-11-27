193aa30242db294b3c307ecab061b69e77eef5e0
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-27 18:04:33 -0500

    Section 3: Event Driven Code Execution
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-3-Basics/app.js
+++ Section-3-Basics/app.js
@@ -25,14 +25,14 @@ const server = http.createServer((req, res) => {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on('end', () => {
+    return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
       fs.writeFileSync('./message.txt', message);
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      return res.end();
     });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
   }
 
   // If not root page, overridden by redirect request
