19a679ea65861ab11e11e5cd719abde658f43374
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-26 19:56:25 -0500

    Section 3: Redirecting Requests
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-3-Basics/app.js
+++ Section-3-Basics/app.js
@@ -1,16 +1,25 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  // console.log(req.url, req.method, req.headers);
   const url = req.url;
+  const method = req.method;
+
   //   process.exit(); // To exit server
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title><head>');
     res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body>'
+      '<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body>',
     );
     res.write('</html>');
+    return res.end();
+  }
+  // Redirect Request 
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('./message.txt', 'DUMMY');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
     return res.end();
   }
   res.setHeader('Content-Type', 'text/html');
