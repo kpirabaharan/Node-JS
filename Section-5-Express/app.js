ef11b27a8db8d18668c87df69493f40831142871
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2022-11-30 19:42:17 -0500

    Section 5: Added Middleware
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-5-Express/app.js
+++ Section-5-Express/app.js
@@ -4,6 +4,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('In the middleware!');
+  next(); // Allows request to continue to the next middleware
+});
+
+app.use((req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<h1>Hello From Express</h1>');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
