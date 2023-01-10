5df98069220ac73aa05db1026588b4c73a464540
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-01-09 19:17:40 -0500

    Section 5: Added Error Page
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-5-Express/app.js
+++ Section-5-Express/app.js
@@ -11,4 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+// Error Page for if nothing is catched
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
+});
+
 app.listen(3000);
