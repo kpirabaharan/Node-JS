0da27c476a937106e7b02b9ca24f6d5a00097f9f
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-01-10 23:41:48 -0500

    Section 5: Served HTML Pages
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-5-Express/app.js
+++ Section-5-Express/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -8,12 +9,12 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/admin',adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 // Error Page for if nothing is catched
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+  res.sendFile(path.join(__dirname, 'views', 'error.html'));
 });
 
 app.listen(3000);
