34427ac827cf215516d6e6a8f1aa53107119eee6
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-01-15 15:55:56 -0500

    Section 7: Saved Data to FS
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-7-MVC/controllers/products.js
+++ Section-7-MVC/controllers/products.js
@@ -17,10 +17,11 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-  res.render('shop', {
-    pageTitle: 'My Shop',
-    path: '/',
-    prods: products,
+  Product.fetchAll((products) => {
+    res.render('shop', {
+      pageTitle: 'My Shop',
+      path: '/',
+      prods: products,
+    });
   });
 };
--- Section-7-MVC/models/product.js
+++ Section-7-MVC/models/product.js
@@ -1,4 +1,8 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../util/path');
+
+const p = path.join(rootDir, 'data', 'products.json');
 
 module.exports = class Product {
   constructor(title, price, description) {
@@ -8,10 +12,24 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
   }
 };
