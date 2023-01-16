7c25640769b985d89124f979d020f7c3d864afb9
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-01-16 16:52:49 -0500

    Section 9: Added Cart Model with Adding Products Fn
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ Section-9-Dynamic-Routes-Advanced-Models/models/cart.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(path.dirname(require.main.filename), 'data', 'cart.json');
+
+module.exports = class Cart {
+  static addProduct(id, price) {
+    // Fetch previous cart
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      // Analyze cart => Find existing product
+      const existingProductIndex = cart.products.findIndex(
+        (prod) => prod.id === id,
+      );
+      const existingProduct = cart.products[existingProductIndex];
+      let updatedProduct;
+      // Add new product or increase quanity
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.quantity = updatedProduct.quantity + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProduct] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, quantity: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+      cart.totalPrice = cart.totalPrice + +price;
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
+      });
+    });
+  }
+};
