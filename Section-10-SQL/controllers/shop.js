d922d6cdbcd01ce68dc6f7c3cd75ba51c96eb9f5
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-01-18 10:29:13 -0500

    Section 10 - SQL Introduction Complete: Fetching Single Product
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-10-SQL/controllers/shop.js
+++ Section-10-SQL/controllers/shop.js
@@ -31,15 +31,16 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  console.log(
-    Product.findById(prodId, (product) => {
+  Product.findById(prodId)
+    .then(([product]) => {
+      console.log(product[0]);
       res.render('shop/product-detail', {
-        product: product,
+        pageTitle: product[0].title,
         path: '/products',
-        pageTitle: product.title,
+        product: product[0],
       });
-    }),
-  );
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
