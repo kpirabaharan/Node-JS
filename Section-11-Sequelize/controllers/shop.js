b5a79e9dda49703bb2f96e3ea8c22bfe3ac50827
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-01-21 23:59:04 -0500

    Section 11 Complete: Resetting Cart and Outputting Orders
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-11-Sequelize/controllers/shop.js
+++ Section-11-Sequelize/controllers/shop.js
@@ -122,17 +122,25 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders',
-  });
+  req.user
+    .getOrders({ include: ['products'] })
+    .then((orders) => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postOrder = (req, res, next) => {
   let fetchedProducts;
+  let fetchedCart;
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
@@ -148,14 +156,10 @@ exports.postOrder = (req, res, next) => {
       );
     })
     .then((_) => {
+      return fetchedCart.setProducts(null);
+    })
+    .then((_) => {
       res.redirect('/orders');
     })
     .catch((err) => console.log(err));
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout',
-  });
 };
--- Section-11-Sequelize/routes/shop.js
+++ Section-11-Sequelize/routes/shop.js
@@ -21,6 +21,4 @@ router.get('/orders', shopController.getOrders);
 
 router.post('/create-order', shopController.postOrder);
 
-router.get('/checkout', shopController.getCheckout);
-
 module.exports = router;
