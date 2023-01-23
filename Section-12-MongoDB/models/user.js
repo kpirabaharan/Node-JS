96988bce23569cfab3c1b64812b9a8c6ecbfcecb
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-01-23 00:44:28 -0500

    Section 12: Cart
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-12-MongoDB/models/user.js
+++ Section-12-MongoDB/models/user.js
@@ -2,14 +2,31 @@ const mongoDb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, _id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = _id;
   }
 
   save() {
     const db = getDb();
     return db.collection('users').insertOne(this);
+  }
+
+  addToCart(product) {
+    const db = getDb();
+    // const cartProduct = this.cart.items.findIndex((cp) => {
+    //   return cp._id === product.id;
+    // });
+    product.quantity = 1;
+    const updatedCart = { item: [product] };
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new mongoDb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } },
+      );
   }
 
   static async findById(userId) {
