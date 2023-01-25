d2757cc8b76d6aac5bf6a1da0a9283748f6335ad
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-01-24 20:53:13 -0500

    Section 12: Added Relational Data to Orders
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-12-MongoDB/models/user.js
+++ Section-12-MongoDB/models/user.js
@@ -80,9 +80,18 @@ class User {
 
   addOrder() {
     const db = getDb();
-    return db
-      .collection('orders')
-      .insertOne(this.cart)
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: new mongoDb.ObjectId(this._id),
+            name: this.name,
+            email: this.email,
+          },
+        };
+        return db.collection('orders').insertOne(order);
+      })
       .then((result) => {
         this.cart = { items: [] };
         return db
@@ -93,6 +102,8 @@ class User {
           );
       });
   }
+
+  getOrders() {}
 
   static async findById(userId) {
     const db = getDb();
