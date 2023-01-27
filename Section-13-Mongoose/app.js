b0f541154a62403c0712e9b037a3d7d96d992820
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-01-26 23:46:04 -0500

    Section 13: Creating User Collection
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-13-Mongoose/app.js
+++ Section-13-Mongoose/app.js
@@ -7,7 +7,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
 
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -17,16 +17,16 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('63ce13b89d028165c8bc5794')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// });
+app.use((req, res, next) => {
+  User.findById('63d356eec5ea0d7fbd989ce7')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -38,9 +38,20 @@ mongoose
   .connect(
     'mongodb+srv://kpirabaharan:7tTH4fUSTDZ4fcz@node-nosql.b4w22at.mongodb.net/shop?retryWrites=true&w=majority',
   )
-  .then(res => {
-    app.listen(3000);
-  })
-  .catch(err => {
-    console.log(err);
+  .then(async res => {
+    try {
+      const existingUser = await User.findOne();
+      if (!existingUser) {
+        const user = new User({
+          name: 'Keeshigan',
+          email: 'Keeshigan@test.com',
+          cart: { items: [] },
+        });
+        user.save();
+      }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      app.listen(3000);
+    }
   });
