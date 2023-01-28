084a505b61aa2624476e5170b8baa99280d32411
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-01-28 18:14:33 -0500

    Section 14: Created Login Page
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-14-Sessions-Cookies/app.js
+++ Section-14-Sessions-Cookies/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 const errorController = require('./controllers/error');
 
 const User = require('./models/user');
@@ -30,6 +31,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 // Error Page for if nothing is catched
 app.use(errorController.error);
