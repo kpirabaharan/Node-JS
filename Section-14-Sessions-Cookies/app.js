9d0fb28561b788375b358999b2527fceb0e64b53
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-01-28 18:05:52 -0500

    Section 14: Sessions and Cookies Project Created
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

---
+++ Section-14-Sessions-Cookies/app.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
+
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('63d489878ad3d1fbece5da7f')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+// Error Page for if nothing is catched
+app.use(errorController.error);
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect('mongodb://localhost:27017/shop').then(async () => {
+  console.log('Connected');
+  try {
+    const existingUser = await User.findOne();
+    if (!existingUser) {
+      const user = new User({
+        name: 'Keeshigan',
+        email: 'Keeshigan@test.com',
+        cart: { items: [] },
+      });
+      user.save();
+    }
+  } catch (err) {
+    console.log(err);
+  } finally {
+    app.listen(3000);
+  }
+});
---
+++ Section-14-Sessions-Cookies/controllers/admin.js
@@ -0,0 +1,103 @@
+const Product = require('../models/product');
+
+exports.getProducts = async (req, res, next) => {
+  try {
+    // const products = await Product.find().select('price -_id').populate('userId');
+    const products = await Product.find();
+    res.render('admin/products', {
+      pageTitle: 'Admin Products',
+      path: '/admin/products',
+      prods: products,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false,
+  });
+};
+
+exports.postAddProduct = async (req, res, next) => {
+  const title = req.body.title;
+  const price = req.body.price;
+  const description = req.body.description;
+  const imageUrl = req.body.imageUrl;
+
+  const product = new Product({
+    title,
+    price,
+    description,
+    imageUrl,
+    userId: req.user,
+  });
+
+  try {
+    await product.save();
+  } catch (err) {
+    console.log(err);
+  } finally {
+    console.log('Created Product');
+    res.redirect('/admin/products');
+  }
+};
+
+exports.getEditProduct = async (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+
+  try {
+    const product = await Product.findById(prodId);
+    if (!product) {
+      res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product,
+    });
+  } catch (err) {
+    console.log(err);
+    res.redirect('/');
+  }
+};
+
+exports.postEditProduct = async (req, res, next) => {
+  const prodId = req.body.productId;
+  const title = req.body.title;
+  const price = req.body.price;
+  const imageUrl = req.body.imageUrl;
+  const description = req.body.description;
+
+  try {
+    await Product.findByIdAndUpdate(prodId, {
+      title,
+      price,
+      description,
+      imageUrl,
+    });
+  } catch (err) {
+    console.log(err);
+  } finally {
+    res.redirect('/admin/products');
+  }
+};
+
+exports.postDeleteProduct = async (req, res, next) => {
+  const prodId = req.body.productId;
+  try {
+    await Product.findByIdAndRemove(prodId);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    res.redirect('/admin/products');
+  }
+};
