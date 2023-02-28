2106511a22e87e5ccc93df957cd5371f506f2588
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-02-27 23:10:03 -0500

    Added Protection to Post Actions from Non-Admin Users
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- Section-17-Adv-Auth/controllers/admin.js
+++ Section-17-Adv-Auth/controllers/admin.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 exports.getProducts = async (req, res, next) => {
   try {
     // const products = await Product.find().select('price -_id').populate('userId');
-    const products = await Product.find();
+    const products = await Product.find({ userId: req.user._id });
     res.render('admin/products', {
       pageTitle: 'Admin Products',
       path: '/admin/products',
@@ -19,7 +19,6 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     editing: false,
-    
   });
 };
 
@@ -64,7 +63,6 @@ exports.getEditProduct = async (req, res, next) => {
       path: '/admin/edit-product',
       editing: editMode,
       product: product,
-      
     });
   } catch (err) {
     console.log(err);
@@ -80,23 +78,25 @@ exports.postEditProduct = async (req, res, next) => {
   const description = req.body.description;
 
   try {
-    await Product.findByIdAndUpdate(prodId, {
-      title,
-      price,
-      description,
-      imageUrl,
-    });
+    await Product.findOneAndUpdate(
+      { _id: prodId, userId: req.user._id },
+      {
+        title,
+        price,
+        description,
+        imageUrl,
+      },
+    );
+    res.redirect('/');
   } catch (err) {
     console.log(err);
-  } finally {
-    res.redirect('/admin/products');
   }
 };
 
 exports.postDeleteProduct = async (req, res, next) => {
   const prodId = req.body.productId;
   try {
-    await Product.findByIdAndRemove(prodId);
+    await Product.deleteOne({ _id: prodId, userId: req.user._id });
   } catch (err) {
     console.log(err);
   } finally {
